12199fa1c9811b4735cce8e17214d6d6a3a90be9
YvesLeBorg <yves@portableehr.com>
2022-12-30 06:46:55 -0500

    removes unused code

--- ehrPatientSDK-ios/Classes/AppState/AppPreferences.m
+++
@@ -1,9 +0,0 @@
-//
-// Created by Yves Le Borgne on 2015-10-02.
-// Copyright (c) 2015-2019 Portable EHR inc. All rights reserved.
-//
-
-#import "AppPreferences.h"
-
-@implementation AppPreferences
-@end
